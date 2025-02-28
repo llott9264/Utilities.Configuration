71ef9852cb660a44b377bc6db28c87630d19731e
Lee Lott <llott88@gmail.com>
2025-02-28 08:23:07 -0600

    #1 - Created MediatR objects and unit tests.

---
+++ src/Utilities.Configuration/MediatR/GetConfigurationByKeyQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Utilities.Configuration.Exceptions;
+
+namespace Utilities.Configuration.MediatR;
+
+public class GetConfigurationByKeyQueryHandler(IConfiguration configuration) : IRequestHandler<GetConfigurationByKeyQuery, string>
+{
+
+	public Task<string> Handle(GetConfigurationByKeyQuery request, CancellationToken cancellationToken)
+	{
+		string? value = configuration.GetValue<string>(request.Key);
+
+		if (string.IsNullOrWhiteSpace(value))
+		{
+			throw new NotFoundException("Configuration Value", request.Key);
+		}
+
+		return Task.FromResult(value);
+	}
+}
