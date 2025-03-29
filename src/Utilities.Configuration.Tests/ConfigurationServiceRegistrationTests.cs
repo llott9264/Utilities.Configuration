dfb3fec2c4944bf6d452179943d9d18c40a87864
Lee Lott <llott88@gmail.com>
2025-03-29 16:45:48 -0500

    #8 - Added tests for registration code.

---
+++ src/Utilities.Configuration.Tests/ConfigurationServiceRegistrationTests.cs
@@ -0,0 +1,100 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Utilities.Configuration.MediatR;
+
+namespace Utilities.Configuration.Tests;
+
+public class ConfigurationServiceRegistrationTests
+{
+	[Fact]
+	public void AddAuthenticationServices_RegistersAllServices_CorrectlyResolvesTypes()
+	{
+		// Arrange
+		ServiceCollection services = new();
+
+		// Act
+		_ = services.AddConfigurationServices();
+		ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+		IMediator? mediator = serviceProvider.GetService<IMediator>();
+
+		// Assert
+		Assert.NotNull(mediator);
+		_ = Assert.IsType<Mediator>(mediator);
+	}
+
+	[Fact]
+	public void AddAuthenticationServices_ReturnsServiceCollection()
+	{
+		// Arrange
+		ServiceCollection services = new();
+
+		// Act
+		IServiceCollection result = services.AddConfigurationServices();
+
+		// Assert
+		Assert.Same(services, result); // Ensures the method returns the same IServiceCollection
+	}
+
+	[Fact]
+	public void AddApplicationServices_ScopedLifetime_VerifyInstanceWithinScope()
+	{
+		// Arrange
+		ServiceCollection services = new();
+
+		// Act
+		_ = services.AddConfigurationServices();
+		ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+		// Assert
+		using IServiceScope scope = serviceProvider.CreateScope();
+		IMediator? service1 = scope.ServiceProvider.GetService<IMediator>();
+		IMediator? service2 = scope.ServiceProvider.GetService<IMediator>();
+
+		Assert.NotSame(service1, service2);
+	}
+
+	[Fact]
+	public void AddApplicationServices_ScopedLifetime_VerifyInstancesAcrossScopes()
+	{
+		// Arrange
+		ServiceCollection services = new();
+
+		// Act
+		_ = services.AddConfigurationServices();
+		ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+		// Assert
+		IMediator? service1, service2;
+
+		using (IServiceScope scope1 = serviceProvider.CreateScope())
+		{
+			service1 = scope1.ServiceProvider.GetService<IMediator>();
+		}
+
+		using (IServiceScope scope2 = serviceProvider.CreateScope())
+		{
+			service2 = scope2.ServiceProvider.GetService<IMediator>();
+		}
+
+		Assert.NotSame(service1, service2);
+	}
+
+	[Fact]
+	public void AddApplicationServices_CleanUpDirectoryHandler_VerifyMediatorHandlerExists()
+	{
+		// Arrange
+		ServiceCollection services = new();
+
+		// Act
+		_ = services.AddConfigurationServices();
+		List<ServiceDescriptor> serviceDescriptors = services.ToList();
+
+		// Assert
+		ServiceDescriptor? handlerDescriptor = serviceDescriptors.FirstOrDefault(sd =>
+			sd.ServiceType == typeof(IRequestHandler<GetConfigurationByKeyQuery, string>));
+
+		Assert.NotNull(handlerDescriptor);
+		Assert.Equal(ServiceLifetime.Transient, handlerDescriptor.Lifetime);
+	}
+}
